b26f03b1261d0e2a5893c7e8db1bb6418be07715
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-12-12 23:15:44 +0700

    feat: update category status

--- src/store/reducers/category.js
+++ src/store/reducers/category.js
@@ -1,38 +1,16 @@
-import {
-  UNAUTHENTICATE,
-  GET_CATEGORIES,
-  EDIT_CATEGORY,
-  ADD_CATEGORY,
-  DELETE_CATEGORY,
-} from "../actions/types";
+import { UNAUTHENTICATE, GET_CATEGORIES } from "../actions/types";
 
 const initialState = {
   categories: {},
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
-  const { type, categories, selectedId } = action;
+  const { type, categories } = action;
   switch (type) {
     case GET_CATEGORIES:
       return {
         categories: { ...categories },
-      };
-    case EDIT_CATEGORY:
-    case ADD_CATEGORY:
-      const category = action.category;
-      return {
-        ...state,
-        categories: {
-          ...state.categories,
-          [category.id]: category,
-        },
-      };
-    case DELETE_CATEGORY:
-      const newCategories = state.categories;
-      delete newCategories[selectedId];
-      return {
-        ...state,
-        categories: newCategories,
       };
     case UNAUTHENTICATE:
       return initialState;
