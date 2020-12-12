6e94ec8c7e2dde418c4f81735ec74e6385a84d7e
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-12-12 22:12:06 +0700

    fix: add benefit status and remove description

--- src/store/reducers/benefit.js
+++ src/store/reducers/benefit.js
@@ -1,38 +1,19 @@
 import {
     UNAUTHENTICATE,
     GET_BENEFITS,
-    DELETE_BENEFIT,
-    EDIT_BENEFIT,
-    ADD_BENEFIT
   } from "../actions/types";
   
   const initialState = {
     benefits: {},
   };
   
+  // eslint-disable-next-line import/no-anonymous-default-export
   export default function (state = initialState, action) {
-    const { type, benefits, selectedId } = action;
+    const { type, benefits } = action;
     switch (type) {
       case GET_BENEFITS:
         return {
           benefits: { ...benefits },
-        };
-      case EDIT_BENEFIT:
-      case ADD_BENEFIT:
-        const benefit = action.benefit;
-        return {
-          ...state,
-          benefits: {
-            ...state.benefits,
-            [benefit.id]: benefit,
-          },
-        };
-      case DELETE_BENEFIT:
-        const newBenefits = state.benefits;
-        delete newBenefits[selectedId];
-        return {
-          ...state,
-          benefits: newBenefits,
         };
       case UNAUTHENTICATE:
         return initialState;
