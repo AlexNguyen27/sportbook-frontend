f70529d26690871bed879a5958cc65448ba83c11
Thanh Nguyen <thanhnguyen.tnn55@gmail.com>
2020-12-13 10:34:19 +0700

    fix: reply comment

--- src/components/pages/comment/Comment.js
+++ src/components/pages/comment/Comment.js
@@ -17,9 +17,9 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import { GET_ERRORS } from "../../../store/actions/types";
-import Swal from "sweetalert2";
-import { useHistory } from "react-router-dom";
 import AddCommentForm from "./component/AddCommentForm";
+import ReplyIcon from "@material-ui/icons/Reply";
+import Collapse from "@material-ui/core/Collapse";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,6 +31,10 @@ const useStyles = makeStyles((theme) => ({
   small: {
     width: theme.spacing(5),
     height: theme.spacing(5),
+  },
+  childSmall: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
   },
   large: {
     width: theme.spacing(6),
@@ -50,44 +54,52 @@ const Comment = ({
 }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const history = useHistory();
 
-  const [comment, setComment] = useState("");
-  const [loading, setLoading] = useState(false);
   const [editLoading, setEditLoading] = useState(false);
   const [isEdit, setIsEdit] = useState("");
   const [editComment, setEditComment] = useState("");
+
   const [deleteLoading, setDeleteLoading] = useState(false);
   const [deteleId, setDeleteId] = useState("");
+
+  const [replyLoading, setReplyLoading] = useState(false);
+  const [replyData, setReplyData] = useState({
+    isReply: false,
+    parentReplyId: "",
+    replyComment: "",
+  });
+
+  const [viewMoreCmt, setViewMoreCmt] = useState(false);
+  const [viewCmtId, setViewCmtId] = useState(null);
 
   const getFullName = (userData) => {
     return userData.firstName + " " + userData.lastName;
   };
 
-  const onAdd = (e) => {
-    e.preventDefault();
-    if (!isAuthenticated) {
-      Swal.fire({
-        title: `Please login to continue?`,
-        text: "",
-        type: "success",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Login!",
-      }).then((result) => {
-        if (result.value) {
-          history.push("/login");
-        }
-      });
-    } else {
-      if (!!comment.trim()) {
-        setLoading(true);
-        addComment(setLoading, comment, ground.id);
-        setComment("");
-      }
-    }
-  };
+  // const onAdd = (e) => {
+  //   e.preventDefault();
+  //   if (!isAuthenticated) {
+  //     Swal.fire({
+  //       title: `Please login to continue?`,
+  //       text: "",
+  //       type: "success",
+  //       showCancelButton: true,
+  //       confirmButtonColor: "#3085d6",
+  //       cancelButtonColor: "#d33",
+  //       confirmButtonText: "Login!",
+  //     }).then((result) => {
+  //       if (result.value) {
+  //         history.push("/login");
+  //       }
+  //     });
+  //   } else {
+  //     if (!!comment.trim()) {
+  //       setLoading(true);
+  //       addComment(setLoading, comment, ground.id);
+  //       setComment("");
+  //     }
+  //   }
+  // };
 
   const onEditComment = (commentId) => {
     if (!!editComment.trim()) {
@@ -111,126 +123,303 @@ const Comment = ({
   };
 
   const getRating = (userId) => {
-    const found = ratings.find((item) => (item.userId = userId));
+    const found = ratings.find((item) => item.userId === userId);
     if (found) return found.point;
     return 0;
   };
+
+  const { isReply, parentReplyId, replyComment } = replyData;
+
+  const renderChildrenComment = () => {
+    const childrenComment = comments.filter(
+      (item) => item.parentId === viewCmtId
+    );
+    console.log("chilre---------------", comments, viewCmtId);
+    return (
+      <>
+        {childrenComment.map((child) => (
+          <>
+            <Col style={{ borderLeft: "1px solid #e8e8e8" }}>
+              <p className="ml-2">{child.comment}</p>
+            </Col>
+            <Row>
+              <Col
+                xs={"auto"}
+                style={{ alignSelf: "center", paddingRight: "5px" }}
+              >
+                <Avatar
+                  alt="Remy Sharp"
+                  src={child?.user?.avatar || "/static/images/avatar/1.jpg"}
+                  className={classes.childSmall}
+                  style={{ marginLeft: "10px" }}
+                />
+              </Col>
+              <Col xs={"auto"} className="pl-0" style={{ alignSelf: "center" }}>
+                <span className="mb-0">{getFullName(child.user)}</span>
+                <span className="ml-2">
+                  ({moment(child.createdAt).format("DD/MM/YYYY hh:mm:ss A")})
+                </span>
+              </Col>
+              {user.id === child.userId && (
+                <Col xs={"auto"} style={{ alignSelf: "center" }}>
+                  <Tooltip title="Delete this comment">
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => onDelete(child.id)}
+                    >
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </Tooltip>
+                </Col>
+              )}
+            </Row>
+            <hr />
+          </>
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
-      <AddCommentForm />
+      <AddCommentForm handleAction={addComment} />
       <hr></hr>
       <h5>{`${comments.length} ${
         comments.length > 1 ? "Comments" : "Comment"
       }`}</h5>
-      {comments.map((item) => (
-        <Row>
-          <Col xs={1} className="pr-0">
-            <Avatar
-              alt="Remy Sharp"
-              src={item?.user?.avatar || "/static/images/avatar/1.jpg"}
-              className={classes.small}
-            />
-          </Col>
-          <Col xs={8} className="pl-0">
-            <h6 className="mb-0">{getFullName(item.user)}</h6>
-            <p>
-              Comment at: {moment(item.createdAt).format("DD/MM/YYYY HH:mm A")}
-            </p>
-          </Col>
-          {user && user?.id !== item.userId ? null : (
-            <Col xs={3} className="text-right">
-              {isEdit === item.id ? (
-                <>
-                  {editLoading ? (
+      {comments.map(
+        (item) =>
+          !item.parentId && (
+            <Row>
+              <Col xs={1} className="pr-0">
+                <Avatar
+                  alt="Remy Sharp"
+                  src={item?.user?.avatar || "/static/images/avatar/1.jpg"}
+                  className={classes.small}
+                />
+              </Col>
+              <Col xs={8} className="pl-0">
+                <h6 className="mb-0">{getFullName(item.user)}</h6>
+                <p>
+                  Comment at:{" "}
+                  {moment(item.createdAt).format("DD/MM/YYYY HH:mm A")}
+                </p>
+              </Col>
+              {user && user?.id !== item.userId ? null : (
+                <Col xs={3} className="text-right">
+                  {isEdit === item.id ? (
+                    <>
+                      {editLoading ? (
+                        <IconButton>
+                          <CircularProgress color="secondary" size={20} />
+                        </IconButton>
+                      ) : (
+                        <>
+                          <Button
+                            color="primary"
+                            type="submit"
+                            size="small"
+                            onClick={() => onEditComment(item.id)}
+                          >
+                            Save
+                          </Button>
+                          <Button
+                            color="primary"
+                            size="small"
+                            type="submit"
+                            onClick={() => {
+                              setIsEdit("");
+                              setEditComment(item.comment);
+                            }}
+                          >
+                            Cancel
+                          </Button>
+                        </>
+                      )}
+                    </>
+                  ) : (
+                    <Button
+                      color="primary"
+                      size="small"
+                      type="submit"
+                      onClick={() => {
+                        setIsEdit(item.id);
+                        setEditComment(item.comment);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  )}
+                  {deleteLoading && deteleId === item.id ? (
                     <IconButton>
-                      <CircularProgress color="secondary" size={20} />
+                      <CircularProgress color="primary" size={20} />
                     </IconButton>
                   ) : (
-                    <>
-                      <Button
-                        color="primary"
-                        type="submit"
-                        size="small"
-                        onClick={() => onEditComment(item.id)}
+                    <Tooltip title="Delete this comment">
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => onDelete(item.id)}
                       >
-                        Save
-                      </Button>
-                      <Button
-                        color="primary"
-                        size="small"
-                        type="submit"
-                        onClick={() => {
-                          setIsEdit("");
-                          setEditComment(item.comment);
-                        }}
-                      >
-                        Cancel
-                      </Button>
-                    </>
+                        <DeleteIcon fontSize="small" />
+                      </IconButton>
+                    </Tooltip>
                   )}
-                </>
-              ) : (
-                <Button
-                  color="primary"
-                  size="small"
-                  type="submit"
-                  onClick={() => {
-                    setIsEdit(item.id);
-                    setEditComment(item.comment);
-                  }}
-                >
-                  Edit
-                </Button>
+                </Col>
               )}
-              {deleteLoading && deteleId === item.id ? (
-                <IconButton>
-                  <CircularProgress color="primary" size={20} />
-                </IconButton>
-              ) : (
-                <Tooltip title="Delete this comment">
-                  <IconButton
-                    aria-label="delete"
-                    onClick={() => onDelete(item.id)}
-                  >
-                    <DeleteIcon fontSize="small" />
-                  </IconButton>
-                </Tooltip>
-              )}
-            </Col>
-          )}
-          <Col xs={12}>
-            {!getRating(item.userId) ? null : (
-              <Rating name="read-only" size="small" value={5} readOnly />
-            )}
+              <Col xs={12}>
+                {!getRating(item.userId) ? null : (
+                  <Rating
+                    name="read-only"
+                    size="small"
+                    value={getRating(item.userId)}
+                    readOnly
+                  />
+                )}
 
-            {/* <h6>Very good</h6> */}
-            {isEdit === item.id ? (
-              <TextFieldInput
-                label="Give a comment here"
-                id="outlined-multiline-flexible"
-                name="comment"
-                fullWidth
-                defaultValue={item.comment}
-                value={editComment || item.comment || ""}
-                onChange={(e) => setEditComment(e.target.value)}
-                error={errors.editComment || ""}
-                variant="outlined"
-                size="small"
-              />
-            ) : (
-              <p>{item.comment}</p>
-            )}
-            <hr></hr>
-          </Col>
-        </Row>
-      ))}
+                {/* <h6>Very good</h6> */}
+                {isEdit === item.id ? (
+                  <TextFieldInput
+                    label="Give a comment here"
+                    id="outlined-multiline-flexible"
+                    name="comment"
+                    fullWidth
+                    defaultValue={item.comment}
+                    value={editComment || item.comment || ""}
+                    onChange={(e) => setEditComment(e.target.value)}
+                    error={errors.editComment || ""}
+                    variant="outlined"
+                    size="small"
+                  />
+                ) : (
+                  <p>{item.comment}</p>
+                )}
+
+                {/* REPLY COMMENT */}
+                {isReply ? (
+                  <Form className="mb-4" onSubmit={(e) => {}}>
+                    {parentReplyId === item.id && (
+                      <>
+                        {replyLoading ? (
+                          <IconButton>
+                            <CircularProgress color="secondary" size={20} />
+                          </IconButton>
+                        ) : (
+                          <>
+                            <Button
+                              color="primary"
+                              type="submit"
+                              size="small"
+                              onClick={() => {
+                                // ADD COMMENT WITH PARENT ID
+                                if (replyComment.trim()) {
+                                  setReplyLoading(true);
+                                  addComment(
+                                    setReplyLoading,
+                                    replyComment,
+                                    ground.id,
+                                    parentReplyId
+                                  );
+                                  setReplyData({
+                                    isReply: false,
+                                    parentReplyId: null,
+                                    replyComment: "",
+                                  });
+                                }
+                              }}
+                            >
+                              Save
+                            </Button>
+                            <Button
+                              color="primary"
+                              size="small"
+                              type="submit"
+                              onClick={() => {
+                                setReplyData({
+                                  isReply: false,
+                                  parentReplyId: null,
+                                  replyComment: "",
+                                });
+                              }}
+                            >
+                              Cancel
+                            </Button>
+                          </>
+                        )}
+                        <Row>
+                          <Col xs={12}>
+                            <TextFieldInput
+                              label="Give a comment here"
+                              id="outlined-multiline-flexible"
+                              name="replyComment"
+                              fullWidth
+                              value={replyComment || ""}
+                              onChange={(e) => {
+                                if (parentReplyId === item.id) {
+                                  setReplyData({
+                                    ...replyData,
+                                    replyComment: e.target.value,
+                                  });
+                                }
+                              }}
+                              error={errors.replyComment || ""}
+                              variant="outlined"
+                              size="small"
+                            />
+                          </Col>
+                        </Row>
+                      </>
+                    )}
+                  </Form>
+                ) : (
+                  <>
+                    <Button
+                      color="primary"
+                      size="small"
+                      type="submit"
+                      onClick={() => {
+                        setViewMoreCmt(!viewMoreCmt);
+                        setViewCmtId(item.id);
+                      }}
+                    >
+                      {viewMoreCmt && viewCmtId === item.id ? "Hide" : "View more"}
+                    </Button>
+                    <Button
+                      color="primary"
+                      size="small"
+                      type="submit"
+                      startIcon={<ReplyIcon />}
+                      onClick={() => {
+                        setReplyData({
+                          ...replyData,
+                          isReply: true,
+                          parentReplyId: item.id,
+                        });
+                      }}
+                    >
+                      Reply
+                    </Button>
+                  </>
+                )}
+                {viewMoreCmt && viewCmtId === item.id && (
+                  <Collapse in={viewMoreCmt} timeout="auto" unmountOnExit>
+                    {/* <CardContent> */}
+                    {renderChildrenComment()}
+                    {/* <p>hello</p> */}
+                    {/* </CardContent> */}
+                  </Collapse>
+                )}
+                <hr></hr>
+              </Col>
+            </Row>
+          )
+      )}
     </>
   );
 };
 
 const mapStateToProps = (state) => ({
   ground: state.ground.selected_ground,
-  rating: state.rating.ratings,
+  ratings: state.rating.ratings,
   comments: state.ground.selected_ground.comments,
   errors: state.errors,
   auth: state.auth,
